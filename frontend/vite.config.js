a5caab8bd9f55517d6b2bf3d4fe0d95602f67e54
SomaRe <shivatej82@gmail.com>
2025-04-19 18:37:14 +0000

    feat: add more confiuguration for PWA, icons and screenshots

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -17,9 +17,60 @@ export default defineConfig({
           description: 'Personal Shopping List App',
           theme_color: '#605dff',
           background_color: '#1d232a',
+          display_override: ["window-controls-overlay", "standalone"],
           icons: [
-            
+            {
+              src: 'icons/icon_32x32.png',
+              sizes: '32x32',
+              type: 'image/png',
+              purpose: 'any'
+            },
+            {
+              src: 'icons/icon_64x64.png',
+              sizes: '64x64',
+              type: 'image/png',
+              purpose: 'any'
+            },
+            {
+              src: 'icons/icon_128x128.png',
+              sizes: '128x128',
+              type: 'image/png',
+              purpose: 'any'
+            },
+            {
+              src: 'icons/icon_256x256.png',
+              sizes: '256x256',
+              type: 'image/png',
+              purpose: 'any'
+            },
+            {
+              src: 'icons/icon_512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'any'
+            },
+            {
+              src: 'icons/icon_1024x1024.png',
+              sizes: '1024x1024',
+              type: 'image/png',
+              purpose: 'any'
+            }
           ]
+          ,
+          screenshots: [
+            {
+              src: 'screenshots/desktop.png',
+              sizes: '3831x1973',
+              type: 'image/png',
+              form_factor: 'wide'
+            },
+            {
+              src: 'screenshots/mobile.png',
+              sizes: '563x1010',
+              type: 'image/png',
+              form_factor: 'narrow'
+            }
+          ],
         },
         registerType: 'autoUpdate',
         devOptions: {
